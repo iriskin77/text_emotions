7a412e39fd3b12bc1df7c615fd066be311faa3fe
iriskin77 <talcore@yandex.ru>
2024-01-06 00:18:42 +0500

    lstm model and a function to process files were uploaded

--- text_emotions/ml_api/apps.py
+++ text_emotions/ml_api/apps.py
@@ -1,6 +1,17 @@
+import json
 from django.apps import AppConfig
-
+from keras.models import load_model
+from keras.preprocessing.text import Tokenizer, tokenizer_from_json
+from pathlib import Path
 
 class MlApiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "ml_api"
+
+    abs_path = Path(__file__).resolve().parent.parent
+
+    model = load_model(rf"{abs_path}/texts_emotions/model/best_model_LSTM10000_2.h5")
+
+    with open(rf'{abs_path}/texts_emotions/model/tokenizer_json.json') as file:
+        data = json.load(file)
+        tokenizer = tokenizer_from_json(data)
---
+++ text_emotions/ml_api/utils.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from pymystem3 import Mystem
+from keras.utils import pad_sequences
+from .apps import MlApiConfig
+
+def process_file_data(path: str, name_column: str):
+
+    try:
+        data_comments = pd.read_excel(path)[name_column]
+        model = MlApiConfig.model
+        tokenizer = MlApiConfig.tokenizer
+        m = Mystem()
+        commentaries = []
+        score = []
+        marks = []
+
+        for comment_index in range(len(data_comments)):
+            if type(data_comments[comment_index]) == str:
+                lst_lemm = m.lemmatize(data_comments[comment_index])
+                prep = [word.strip(r'!?"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n').lower() for word in lst_lemm if word.isalpha()]
+                sequence = tokenizer.texts_to_sequences([' '.join(prep)])
+                sequence = pad_sequences(sequence, 64)
+                score = model.predict(sequence)
+
+                if score[[0]] > 0.9:
+                    commentaries.append(data_comments[comment_index])
+                    marks.append('Отрицательный')
+                    score.append(score)
+
+                elif 0.9 > score[[0]] > 0.4:
+                    commentaries.append(data_comments[comment_index])
+                    marks.append('Нейтральный')
+                    score.append(score)
+
+                else:
+                    commentaries.append(data_comments[comment_index])
+                    marks.append('Положительный')
+                    score.append(score)
+
+        data = pd.read_excel(path)
+        headers = list(data.columns.values)
+        data.drop(headers, axis=1, inplace=True)
+        data['Сообщение'] = commentaries
+        data['Класс'] = marks
+        data['Коэффициент'] = score
+
+        return data, True
+
+    except:
+
+        return None, False
+
+
