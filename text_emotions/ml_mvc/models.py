f8b44a5a46e0694c7f8e5aef539be659cc15a189
iriskin77 <talcore@yandex.ru>
2024-01-06 00:09:20 +0500

    models uploaded for the apps ml_api and ml_mvc

---
+++ text_emotions/ml_mvc/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.core.validators import FileExtensionValidator
+# Create your models here.
+
+class FileModel(models.Model):
+
+    name_file = models.CharField(max_length=255, verbose_name="Название файла")
+    author_file = models.CharField(max_length=255, blank=True, verbose_name="Автор файла")
+    name_column = models.CharField(max_length=255, null=False, verbose_name="Название колонки")
+    file = models.FileField(upload_to="files/", validators=[FileExtensionValidator(['xlsx'])], verbose_name="Файл")
+    status = models.BooleanField(default=False, verbose_name="Статус обработки")
+    date = models.DateTimeField(auto_now_add=True, verbose_name="Дата")
+
+    def __str__(self):
+        return self.name_file
