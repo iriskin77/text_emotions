23f0e15434c799c763823ec8a64dab31f31c4005
iriskin77 <talcore@yandex.ru>
2024-01-13 14:09:12 +0500

    Changes: some names of lists were changed

--- text_emotions/ml_api/utils.py
+++ text_emotions/ml_api/utils.py
@@ -3,51 +3,45 @@ from pymystem3 import Mystem
 from keras.utils import pad_sequences
 from .apps import MlApiConfig
 
-def process_file_data(path: str, name_column: str):
 
-    try:
-        data_comments = pd.read_excel(path)[name_column]
-        model = MlApiConfig.model
-        tokenizer = MlApiConfig.tokenizer
-        m = Mystem()
-        commentaries = []
-        score = []
-        marks = []
+def process_file_data(path, name_column):
 
-        for comment_index in range(len(data_comments)):
-            if type(data_comments[comment_index]) == str:
-                lst_lemm = m.lemmatize(data_comments[comment_index])
-                prep = [word.strip(r'!?"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n').lower() for word in lst_lemm if word.isalpha()]
-                sequence = tokenizer.texts_to_sequences([' '.join(prep)])
-                sequence = pad_sequences(sequence, 64)
-                score = model.predict(sequence)
+    data_comments = pd.read_excel(path)[name_column]
+    model = MlApiConfig.model
+    tokenizer = MlApiConfig.tokenizer
+    m = Mystem()
+    commentaries = []
+    score = []
+    marks = []
 
-                if score[[0]] > 0.9:
-                    commentaries.append(data_comments[comment_index])
-                    marks.append('Отрицательный')
-                    score.append(score)
+    for comment_index in range(len(data_comments)):
+        if type(data_comments[comment_index]) == str:
+            lst_lemm = m.lemmatize(data_comments[comment_index])
+            prep = [word.strip(r'!?"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n').lower() for word in lst_lemm if word.isalpha()]
+            sequence = tokenizer.texts_to_sequences([' '.join(prep)])
+            sequence = pad_sequences(sequence, 64)
+            score = model.predict(sequence)
 
-                elif 0.9 > score[[0]] > 0.4:
-                    commentaries.append(data_comments[comment_index])
-                    marks.append('Нейтральный')
-                    score.append(score)
+            if score[[0]] > 0.9:
+                commentaries.append(data_comments[comment_index])
+                marks.append('Отрицательный')
+                score.append(score)
 
-                else:
-                    commentaries.append(data_comments[comment_index])
-                    marks.append('Положительный')
-                    score.append(score)
+            elif 0.9 > score[[0]] > 0.4:
+                commentaries.append(data_comments[comment_index])
+                marks.append('Нейтральный')
+                score.append(score)
 
-        data = pd.read_excel(path)
-        headers = list(data.columns.values)
-        data.drop(headers, axis=1, inplace=True)
-        data['Сообщение'] = commentaries
-        data['Класс'] = marks
-        data['Коэффициент'] = score
+            else:
+                commentaries.append(data_comments[comment_index])
+                marks.append('Положительный')
+                score.append(score)
 
-        return data, True
+    data = pd.read_excel(path)
+    headers = list(data.columns.values)
+    data.drop(headers, axis=1, inplace=True)
+    data['Сообщение'] = commentaries
+    data['Класс'] = marks
+    data['Коэффициент'] = score
 
-    except:
-
-        return None, False
-
-
+    return data
